5ef5b84a23de96d9a7267ceabfe0ef29be700b04
Eskandar Hany Ayad <hanyayad71@gmail.com>
2023-09-02 10:58:56 +0200

    Adding Docker Volumes to presist data after db containers restart

--- app/server.js
+++ app/server.js
@@ -23,10 +23,10 @@ app.get("/profile-picture", function (req, res) {
 });
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://adming:admin@localhost:27017";
+let mongoUrlLocal = "mongodb://root:root@localhost:27017";
 
 // use when starting application as docker container
-let mongoUrlDocker = "mongodb://adming:admin@mongodb";
+let mongoUrlDocker = "mongodb://root:root@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
